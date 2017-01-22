ef64b9272aaf9323a59e7870af146bbc46bd626c
entcheva <christina.entcheva@gmail.com>
2017-01-21 23:02:39 -0500

    Done.

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ function nestedTarget() {
 };
 
 function deepestChild() {
-
+  let divs = document.querySelectorAll('#grand-node div')
+  return divs[divs.length - 1]
 };
 
-function increaseRankBy(n) {
 
+function increaseRankBy(n) {
+  let elements = document.querySelectorAll('.ranked-list li')
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = parseInt(elements[i].innerHTML) + n
+  }
 };
